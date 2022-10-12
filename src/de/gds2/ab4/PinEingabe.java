dcbcdf7101b4c0353e2c523c37db14fd20fd1514
GreenM4x <greenm4x@gmail.com>
2022-10-12 14:54:03 +0200

    Commit

---
+++ src/de/gds2/ab4/PinEingabe.java
@@ -0,0 +1,49 @@
+package de.gds2.ab4;
+
+import java.util.Scanner;
+
+public class PinEingabe {
+    public static int fixPin = 1337;
+    public static int fixPuk = 4242;
+
+    public static void main(String[] args) {
+
+        int tries = 0;
+        Scanner inputScanner = new Scanner(System.in);
+        int inputPin;
+        int inputPuk;
+
+        System.out.print("geben sie ihren 4 Stelligen Pin ein: ");
+
+         do {
+             inputPin = inputScanner.nextInt();
+             if (inputPin > 1000 && inputPin < 9999){
+                 tries++;
+                 if(inputPin != fixPin && tries < 3) {
+                     System.out.println("try again.");
+                 }
+                 if (inputPin == fixPin) {
+                     System.out.println("Der PIN ist Ricthig");
+                 }
+                 if (inputPin != fixPin && tries == 3) {
+                     System.out.println("Sie haben den PIN dreimal falsch eingegeben!");
+                 }
+
+             } else {
+                 System.out.println("*hust* *hust* 4 stellen");
+             }
+        } while (inputPin != fixPin && tries < 3);
+
+        System.out.println("Geben sie jetzt den Puk ein: ");
+
+         do {
+             inputPuk = inputScanner.nextInt();
+                if (inputPuk != fixPuk){
+                    System.out.println("Try again.");
+                } else {
+                    System.out.println("Yey Puk ist richtig!");
+                }
+         } while (inputPuk != fixPuk);
+
+    }
+}
--- src/de/gds2/ab4/pin_eingabe.java
+++
@@ -1,7 +0,0 @@
-package de.gds2.ab4;
-
-public class pin_eingabe {
-    public static void main(String[] args) {
-        
-    }
-}
