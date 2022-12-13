ac0c84873d8ce2f54aa9dac5ca08aab02cdb86be
GreenM4x <greenm4x@gmail.com>
2022-12-13 08:24:29 +0100

    Commit

---
+++ src/de/gds2/Abgabe/Vergleich.java
@@ -0,0 +1,13 @@
+package de.gds2.Abgabe;
+
+import java.util.Arrays;
+
+public class Vergleich {
+
+    public static void main(String[] args) {
+        int[] zahlen = {3,5,8,6,4,1,2,7,9};
+        Arrays.sort(zahlen);
+        System.out.println(Arrays.toString(zahlen)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+}
+
