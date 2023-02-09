950a4dbef1ed3cb71d024e86b90523b880115127
GreenM4x <greenm4x@gmail.com>
2023-02-09 10:59:15 +0100

    Commit

--- src/de/gds2/pr1_x280_x290_Vererbung/StartMitarbeiter.java
+++ src/de/gds2/pr1_x280_x290_Vererbung/StartMitarbeiter.java
@@ -8,6 +8,7 @@ public class StartMitarbeiter {
         Mitarbeiter Wa = new Mitarbeiter("Wellstein", "Andreas", 1600.50);
         Mitarbeiter Dit = new Mitarbeiter("Dietrich", "Hans-Martin", 1650.00);
         Fuehrungskraft Krp = new Fuehrungskraft("Krappel", "Joachim", 4000.00);
+        Auszubildender Svn = new Auszubildender("Groot", "IM", 1337, "Baum", 42);
 
 
         System.out.println(Krp.name + " is " + Krp.posistion);
