686cd87b2d3c434e287e2162091ef555777533dd
GreenM4x <greenm4x@gmail.com>
2023-02-14 15:14:46 +0100

    Vererbung

--- src/de/gds2/pr1_x280_x290_Vererbung/Bank/GiroKonto.java
+++ src/de/gds2/pr1_x280_x290_Vererbung/Bank/GiroKonto.java
@@ -1,7 +1,7 @@
 package de.gds2.pr1_x280_x290_Vererbung.Bank;
 
 public class GiroKonto extends Bankkonto {
-    private double gebuehr;
+    private double gebuehr = 0.05;
 
     public GiroKonto(double kontoStand, double gebuehr) {
         super(kontoStand);
@@ -11,10 +11,12 @@ public class GiroKonto extends Bankkonto {
     @Override
     public void abheben(double x) {
         super.abheben(x);
+        kontoStand -= gebuehr;
     }
 
     @Override
     public void einzahlen(double x) {
         super.einzahlen(x);
+        kontoStand -= gebuehr;
     }
 }
